5968e6658a275f57906e37a3fe56bd84edf2fd82
Linh Duong <linhdn1198@gmail.com>
2019-08-25 10:23:15 +0700

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$numbersA = [1, 2, 3, 4, 5, 6];
+$numbersB = [1.1 , 1.2, 1.3, 1.4, 1.5, 1.6];
+$numbersC = [];
+
+for ($i=0; $i < count($numbersA); $i++) { 
+    $tempt = [$numbersA[$i], $numbersB[$i]];
+    array_push($numbersC, $tempt);    
+}
+
+print_r($numbersC);
